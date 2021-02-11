d053090a9d75104ad3ee7e2bb2d4f6baee906f5a
Mushfiqur Anik <43996786+mushfiqur-anik@users.noreply.github.com>
2021-02-10 23:57:27 -0500

    Initial Navbar

---
+++ src/Components/Practice.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Practice = () => {
+    return (
+        <div>
+            <h1> In the Practice </h1>
+            
+        </div>
+    )
+}
+
+export default Practice
