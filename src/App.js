ed529d7631ef482069d2167dd4400c742c145677
Mushfiqur Anik <43996786+mushfiqur-anik@users.noreply.github.com>
2021-02-12 23:18:32 -0500

    Added Services

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./Components/Navbar"
 import Home from "./Components/Home"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Services from "./Components/Services"
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Services />
     </div>
   );
 }
